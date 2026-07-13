09d176f917712c16cb378b7037bd91136f40e8ca
zys <zengyishan@rongxinzy.com>
2026-07-13 11:15:02 +0800

    fix(ui): align select dropdown to start matching input left edge

--- src/shared/components/ui/select.tsx
+++ src/shared/components/ui/select.tsx
@@ -60,7 +60,7 @@ function SelectContent({
   children,
   side = "bottom",
   sideOffset = 4,
-  align = "center",
+  align = "start",
   alignOffset = 0,
   alignItemWithTrigger = true,
   ...props
