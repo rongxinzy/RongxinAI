f1f50bbb445b0eae4a13b6a2aa67b6eb080ecc49
zys <zengyishan@rongxinzy.com>
2026-07-14 15:47:50 +0800

    fix(ui): place TodoQueue and PromptInput in same max-w-5xl container

--- src/renderer/components/cowork/CoworkSessionDetail.tsx
+++ src/renderer/components/cowork/CoworkSessionDetail.tsx
@@ -1222,11 +1222,9 @@ const CoworkSessionDetail: React.FC<CoworkSessionDetailProps> = ({
       {isStreaming && <StreamingBar messages={currentSession.messages} />}
 
       {/* Input Area */}
-      <div className="max-w-5xl min-w-[320px] mx-auto pl-4 w-full">
-        <TodoQueue messages={currentSession.messages} />
-      </div>
       <div className="px-4 pb-4 shrink-0">
         <div className="max-w-5xl min-w-[320px] mx-auto pl-4">
+          <TodoQueue messages={currentSession.messages} />
           <CoworkPromptInput
             ref={promptInputRef}
             onSubmit={onContinue}
