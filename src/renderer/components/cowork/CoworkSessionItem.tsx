244599b482ab4fe5a85648c0988eb9987dee3432
unknown <wxy>
2026-09-03 15:28:38 +0800

    feat(模型设置): 使用Agent润色模型设置的页面呈现

--- src/renderer/components/cowork/CoworkSessionItem.tsx
+++ src/renderer/components/cowork/CoworkSessionItem.tsx
@@ -396,6 +396,8 @@ const CoworkSessionItem: React.FC<CoworkSessionItemProps> = ({
         description={i18nService.t('deleteTaskConfirmMessage')}
         cancelLabel={i18nService.t('cancel')}
         confirmLabel={i18nService.t('deleteSession')}
+        cancelVariant="outline"
+        confirmVariant="outline"
         onCancel={handleCancelDelete}
         onConfirm={handleConfirmDelete}
       />
